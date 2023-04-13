ffe820b47aecdcc7da826471f587e58f10ce87cc
meticulousdev <meticulousdev@gmail.com>
2023-04-13 10:05:47 +0900

    Inference Type

--- tutorial/variables.go
+++ tutorial/variables.go
@@ -7,14 +7,26 @@ import (
 
 func main() {
 	var i, j int = 1, 2
-	k := 3
-	c, python, java := true, false, "no!"
-
-	fmt.Println(i, j, k, c, python, java)
+	fmt.Println(i, j)
 	fmt.Println(reflect.TypeOf(i))
 	fmt.Println(reflect.TypeOf(j))
-	fmt.Println(reflect.TypeOf(k))
+	fmt.Println()
+
+	c, python, java := true, false, "no!"
+	fmt.Println(c, python, java)
 	fmt.Println(reflect.TypeOf(c))
 	fmt.Println(reflect.TypeOf(python))
 	fmt.Println(reflect.TypeOf(java))
+	fmt.Println()
+
+	// Inference Type
+	k := 3
+	var zero_value int
+	const pi = 3.14
+	// pi = 0 // cannot assign to pi (untyped float constant 3.14)
+	fmt.Printf("Type: %T Value: %v\n", k, k)
+	fmt.Println(reflect.TypeOf(k))
+	fmt.Printf("Type: %T Value: %v\n", zero_value, zero_value)
+	fmt.Printf("Type: %T Value: %v\n", pi, pi)
+	fmt.Println()
 }
