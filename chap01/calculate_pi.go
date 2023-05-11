a39cb97b52435a4b027178ab05ac2fb1f5cca676
meticulousdev <meticulousdev@gmail.com>
2023-05-11 11:23:08 +0900

    pi

---
+++ chap01/calculate_pi.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func calculate_py(n_term int) float64 {
+	numerator := 4.0
+	denominator := 1.0
+	operation := 1.0
+	v_pi := 0.0
+
+	for i := 1; i < n_term; i++ {
+		v_pi += operation * (numerator / denominator)
+		denominator += 2.0
+		operation *= -1.0
+	}
+
+	return v_pi
+}
+
+func main() {
+	start := time.Now()
+	fmt.Print("PI                  : ")
+	fmt.Println(calculate_py(1000000))
+	elapsed := time.Since(start)
+	fmt.Printf("PI calculation took : %s\n\n", elapsed)
+}
