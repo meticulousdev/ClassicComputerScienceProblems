0fd09a83657cd72e275a59d3d5ef59d77b1d1b9d
meticulousdev <meticulousdev@gmail.com>
2023-05-17 23:54:09 +0900

    compression

--- chap01/trivial_compression.go
+++ chap01/trivial_compression.go
@@ -2,16 +2,47 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-func compression() {
+func compression(gene []string) uint {
+	var bit_string uint = 0b01
 
+	for _, nucleotide := range gene {
+		bit_string <<= 2
+		if strings.Compare(nucleotide, "A") == 0 {
+			bit_string |= 0b00
+		} else if strings.Compare(nucleotide, "C") == 0 {
+			bit_string |= 0b01
+		} else if strings.Compare(nucleotide, "G") == 0 {
+			bit_string |= 0b10
+		} else if strings.Compare(nucleotide, "T") == 0 {
+			bit_string |= 0b11
+		} else {
+			fmt.Printf("Invalid nucleotide:%s", nucleotide)
+		}
+	}
+
+	return bit_string
 }
 
-func main() {
-	fmt.Println("Compression Test")
+// func decompress(bit_string uint) []string {
+// 	gene := []string{}
+// 	for i := 0; i < bits.Len(bit_string); i++ {
 
-	var a uint = 1
-	a <<= 2
-	fmt.Println(a)
+// 	}
+// }
+
+func main() {
+	const LENGTH int = 10
+	gene := []string{"A", "G", "C", "T"}
+	original := [LENGTH]string{}
+	for i := 0; i < LENGTH; i++ {
+		original[i] = gene[i%4]
+	}
+	fmt.Printf("%s", original)
+	fmt.Println()
+
+	var bit_string = compression(gene)
+	fmt.Printf("%b \n", bit_string)
 }
