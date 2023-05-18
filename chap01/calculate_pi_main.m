ac6dad6d5a7a82e854f304532231190f7f9af985
meticulousdev <meticulousdev@gmail.com>
2023-05-18 23:55:19 +0900

    calculate pi

---
+++ chap01/calculate_pi_main.m
@@ -0,0 +1,16 @@
+tic
+fprintf("%2.60f \n", calculate_pi(1e7))
+toc
+
+function v_pi = calculate_pi(n_term)
+    numerator = 4.0;
+    denominator = 1.0;
+    operation = 1.0;
+    v_pi = 0.0;
+
+    for i = 0 : n_term
+        v_pi = v_pi + operation * (numerator / denominator);
+        denominator = denominator + 2.0;
+        operation = operation * (- 1.0);
+    end
+end
