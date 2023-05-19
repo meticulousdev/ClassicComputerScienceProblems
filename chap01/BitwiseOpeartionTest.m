166d7211ad84dc581f4993e43f16fa42562f4c78
meticulousdev <meticulousdev@gmail.com>
2023-05-19 11:02:14 +0900

    OR operation test

--- chap01/BitwiseOpeartionTest.m
+++ chap01/BitwiseOpeartionTest.m
@@ -3,27 +3,28 @@ for i = 1 : 9
     gene = append(gene, 'AGCT');
 end
 
-fprintf("gene          : %s\n", gene)
-fprintf("gene length   : %d\n", length(gene))
+fprintf("gene          : %s\n", gene);
+fprintf("gene length   : %d\n", length(gene));
 
 bit_string = 1;
+fprintf("%d %s\n", bit_string, dec2bin(bit_string));
 
-for i = 1 : length(gene)
-    nucleotide = gene(i);
-    bit_string = bitshift(bit_string, 2);
+bit_string = bitshift(bit_string, 1);
+fprintf("%d %s\n", bit_string, dec2bin(bit_string));
 
-    if nucleotide == 'A'
-        bit_string = bitor(bit_string, 0b00);
-    elseif nucleotide == 'C'
-        bit_string = bitor(bit_string, 0b01);
-    elseif nucleotide == 'G'
-        bit_string = bitor(bit_string, 0b10);
-    elseif nucleotide == 'T'
-        bit_string = bitor(bit_string, 0b11);
-    else
-        error("Invalid nucleotide: %s", nucleotide);
-    end
+bit_string = 1;
+bit_string_ret = bitor(bit_string, 0b00);
+fprintf("%s | %s = %s\n", dec2bin(bit_string), dec2bin(0b00), dec2bin(bit_string_ret));
+% 10 | 00 = 10
 
-    fprintf("%s", nucleotide)
-    fprintf("%s", bit_string)
-end
+bit_string_ret = bitor(bit_string, 0b01);
+fprintf("%s | %s = %s\n", dec2bin(bit_string), dec2bin(0b01), dec2bin(bit_string_ret));
+% 10 | 01 = 11
+
+bit_string_ret = bitor(bit_string, 0b10);
+fprintf("%s | %s = %s\n", dec2bin(bit_string), dec2bin(0b10), dec2bin(bit_string_ret));
+% 10 | 10 = 10
+
+bit_string_ret = bitor(bit_string, 0b11);
+fprintf("%s | %s = %s\n", dec2bin(bit_string), dec2bin(0b11), dec2bin(bit_string_ret));
+% 10 | 11 = 11
